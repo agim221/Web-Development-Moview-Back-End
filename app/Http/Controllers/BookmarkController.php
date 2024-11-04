3156b27a9e11defdc5a445d32183cbb1aa7027c2
ZahranAn <123435005+ZahranAn@users.noreply.github.com>
2024-11-04 11:36:32 +0700

    coba bookmark

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -30,4 +30,43 @@ class BookmarkController extends Controller
         $bookmarks = $user->bookmarks()->with('film')->skip($start)->take($end - $start)->get();
         return response()->json($bookmarks);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'film_id' => 'required|exists:films,id',
+        ]);
+
+        $bookmark = Bookmark::create($request->all());
+
+        return response()->json($bookmark, 201);
+    }
+
+    public function show($id)
+    {
+        $bookmark = Bookmark::with('film')->findOrFail($id);
+        return response()->json($bookmark);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'film_id' => 'required|exists:films,id',
+        ]);
+
+        $bookmark = Bookmark::findOrFail($id);
+        $bookmark->update($request->all());
+
+        return response()->json($bookmark);
+    }
+
+    public function destroy($id)
+    {
+        $bookmark = Bookmark::findOrFail($id);
+        $bookmark->delete();
+
+        return response()->json(null, 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,10 @@ Route::get('auth/callback/google', [SocialAuthController::class, 'handleProvider
 
 Route::get('bookmarks/{token}', [BookmarkController::class, 'getFilmBookmark']);
 Route::get('bookmarks/{start}/{end}', [BookmarkController::class, 'getFilmBookmarkByRange']);
+Route::post('bookmarks', [BookmarkController::class, 'store']);
+Route::get('bookmarks/{id}', [BookmarkController::class, 'show']);
+Route::put('bookmarks/{id}', [BookmarkController::class, 'update']);
+Route::delete('bookmarks/{id}', [BookmarkController::class, 'destroy']);
 
 Route::get('/countries', [CountryController::class, 'index']);
 Route::post('/countries', [CountryController::class, 'store']);
